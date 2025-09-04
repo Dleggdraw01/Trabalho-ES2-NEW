8af45ae7b0fb8bac2306a10eda08c715b14b3ed3
Vítor Marques <dleggdraw@gmail.com>
2025-09-04 18:11:30 -0300

    Implementadas funções frear(), subir/descer marcha, menos imprimir!!

--- index.ts
+++ index.ts
@@ -23,7 +23,18 @@ while(true){
         case 1:
             acelerar(carro);
             break;
-    
+        case 2:
+            frear(carro)
+            break;
+        case 3:
+            subirMarcha(carro)
+            break;
+        case 4:
+            descerMarcha(carro)
+            break
+        case 5:
+            imprimirDados(carro)
+        break
         default:
             break;
     }
@@ -34,8 +45,38 @@ console.table(carro);
 function acelerar(veiculo: Veiculo): void{
     if(veiculo.marchaAtual != 0){
     veiculo.velocidade += veiculo.potencia*0.1;
-    console.log(veiculo.velocidade);
-}}
+    console.log(`Velocidade atual: ${veiculo.velocidade.toFixed(1)} km/h`)
+    }else{
+    console.log("O veículo está em ponto morto. Engate uma marcha para acelerar!")
+  }
+}
+
+function frear(veiculo: Veiculo):void{
+    if (veiculo.velocidade > 0){
+        veiculo.velocidade = Math.max(0, veiculo.velocidade - veiculo.potencia * 0.1)
+        console.log(`Velocidade atual: ${veiculo.velocidade.toFixed(1)} km/h`)
+    } else {
+        console.log("O veículo já está parado!")
+    }
+}
+
+function subirMarcha(veiculo:Veiculo):void{
+    if(veiculo.marchaAtual - veiculo.numeroMarchas){
+        veiculo.marchaAtual++
+        console.log(`Marcha atual: ${veiculo.marchaAtual}`)
+    }else{
+        console.log("Já está na marcha mais alta!")
+    }
+}
+
+function descerMarcha(veiculo:Veiculo):void{
+    if (veiculo.marchaAtual > 0){
+        veiculo.marchaAtual--
+        console.log(`Marcha atual: ${veiculo.marchaAtual}`)
+    }else{
+        console.log("Já está em ponto morto!")
+    }
+}
 
 function criaVeiculo(): Veiculo{
     const veiculo: Veiculo = new Veiculo();
